32b40a990b984c75d492036e710f4f78e2b046c6
Jonathan Farbowitz <jfarbowitz@guggenheim.org>
2019-01-08 12:19:20 -0500

    fixed syntax to work with my version of bash

--- general_utilities/batchvalidatebags.sh
+++ general_utilities/batchvalidatebags.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd "$1"
 for f in *; do
-    if [ -d ${f} ]; then
+    if [ -d "${f}" ]; then
         # Will not run if no directories are available
         bagit.py --validate --processes 4 $f
     fi
